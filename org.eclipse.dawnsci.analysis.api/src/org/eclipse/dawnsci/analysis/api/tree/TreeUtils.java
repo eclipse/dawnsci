746efebcbb9909dfb0e713b2b2b11679730f8e6f
Peter Chang <peter.chang@diamond.ac.uk>
2015-04-10 13:42:19 +0100

    Add helper method to find the path to a node in a tree

---
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/tree/TreeUtils.java
@@ -0,0 +1,36 @@
+/*-
+ * Copyright 2015 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.dawnsci.analysis.api.tree;
+
+public class TreeUtils {
+
+	/**
+	 * Get the path
+	 * @param tree
+	 * @param node
+	 * @return path to node, return null if not found
+	 */
+	public static String getPath(Tree tree, Node node) {
+		return getPathDepthFirst(tree.getGroupNode(), node);
+	}
+
+	private static String getPathDepthFirst(final GroupNode group, final Node node) {
+		for (NodeLink l : group) {
+			if (l.getDestination() == node) {
+				return l.getFullName();
+			} else if (l.isDestinationGroup()) {
+				String p = getPathDepthFirst((GroupNode) l.getDestination(), node);
+				if (p != null)
+					return p;
+			}
+		}
+		return null;
+	}
+}
