1d22eb5ac82463503e7570c215ce135f2d907449
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-04-20 15:48:20 +0100

    Add licencing

--- org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/image/ImageThresholdType.java
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/image/ImageThresholdType.java
@@ -1,7 +1,18 @@
+/*-
+ *******************************************************************************
+ * Copyright (c) 2011, 2015 Diamond Light Source Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
 package org.eclipse.dawnsci.analysis.api.image;
 
 /**
  * Thresholding filters
+ * 
+ * @author Baha El Kassaby
  */
 public enum ImageThresholdType {
 	GLOBAL_CUSTOM,
