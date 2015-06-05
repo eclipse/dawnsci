52a49400412987f13787cf1dede025cc25cfdbca
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2015-06-05 13:52:18 +0100

    Switched to using raw double[], much faster.

--- org.eclipse.dawnsci.analysis.dataset/src/org/eclipse/dawnsci/analysis/dataset/impl/SummedAreaTable.java
+++ org.eclipse.dawnsci.analysis.dataset/src/org/eclipse/dawnsci/analysis/dataset/impl/SummedAreaTable.java
@@ -9,17 +9,6 @@
 
 package org.eclipse.dawnsci.analysis.dataset.impl;
 
-import java.io.Serializable;
-import java.text.Format;
-import java.util.List;
-
-import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
-import org.eclipse.dawnsci.analysis.api.dataset.ILazyDataset;
-import org.eclipse.dawnsci.analysis.api.dataset.Slice;
-import org.eclipse.dawnsci.analysis.api.dataset.SliceND;
-import org.eclipse.dawnsci.analysis.api.metadata.IMetadata;
-import org.eclipse.dawnsci.analysis.api.metadata.MetadataType;
-import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.api.roi.IRectangularROI;
 
 /**
@@ -40,11 +29,11 @@ import org.eclipse.dawnsci.analysis.api.roi.IRectangularROI;
  *
  * @author Matthew Gerring
  */
-public class SummedAreaTable implements IDataset {
+public class SummedAreaTable {
 	
-	private int[]         shape; // We cache shape for speed reasons (it is cloned in the dataset on getShape())
+	private int[]         shape;     // We cache shape for speed reasons (it is cloned in the dataset on getShape())
 	private Dataset       image;
-	private DoubleDataset sum, sum2; // Use concrete because use setAbs for speed up.
+	private double[]      sum, sum2; // Use double[] because faster than Dataset
 
 	/**
 	 * Calls SummedAreaTable(Image, false)
@@ -65,6 +54,8 @@ public class SummedAreaTable implements IDataset {
 		this.image = image;
 		this.shape = image.getShape();
 		if (image.getRank()!=2) throw new Exception("You may only get sum table of 2D data!");
+		if (shape[0]<1) throw new Exception("You may only get sum table with image of side > 0");
+		if (shape[1]<1) throw new Exception("You may only get sum table with image of side > 0");
 		createSummedTable(image, willRequireVariance);
 	}
 	
@@ -85,21 +76,21 @@ public class SummedAreaTable implements IDataset {
 	    //Create integral
 		boolean requireSum = false;
 		if (sum == null) {
-			sum  = new DoubleDataset(shape);
+			sum  = new double[shape[0]*shape[1]];
 			requireSum = true;
 		}
 		
 		if (requireSum2 && sum2==null) {
-			sum2  = new DoubleDataset(shape);
+			sum2  = new double[shape[0]*shape[1]];
 		}
 	    
 	    // Create a position iterator
-	    final PositionIterator it = new PositionIterator(shape);
-	    while(it.hasNext()) {
-	    	final int[]  pos   = it.getPos();	    	
-	    	final double value = image.getElementDoubleAbs(get1DIndex(pos[0], pos[1], shape));
-	        if (requireSum)  fill(value,  sum,  pos, shape);
-	        if (requireSum2) fill(Math.pow(value, 2d), sum2, pos, shape);
+	    final int[] pos = new int[]{0,0};
+	    for(int i=0;i<sum.length;++i) {
+	    	final double value = image.getElementDoubleAbs(i);
+	    	fillNDPositionFromShape(i, shape, pos);    	
+	        if (requireSum)  fill(i, value,  sum,  pos, shape);
+	        if (requireSum2) fill(i, Math.pow(value, 2d), sum2, pos, shape);
 	    }
 	}
 
@@ -110,19 +101,19 @@ public class SummedAreaTable implements IDataset {
 	 * @param pos
 	 * @param shape
 	 */
-	private static final void fill(double value, DoubleDataset sum, int[] pos, int[] shape) {
+	private static final void fill(int index, double value, double[] sum, int[] pos, int[] shape) {
     	
 		int x = pos[0];
     	int y = pos[1];
     	
         // I(x,y) = i(x,y) + I(x-1,y) + I(x,y-1) - I(x-1,y-1)
         //Calculate coefficients
-        double sxm  = (x > 0)          ? sum.getElementDoubleAbs(get1DIndex(x-1,y,shape))   : 0;
-        double sym  = (y > 0)          ? sum.getElementDoubleAbs(get1DIndex(x,y-1,shape))   : 0;
-        double sxym = (x > 0 && y > 0) ? sum.getElementDoubleAbs(get1DIndex(x-1,y-1,shape)) : 0;
+        double sxm  = (x > 0)          ? sum[get1DIndexFast(x-1,y,shape)]   : 0;
+        double sym  = (y > 0)          ? sum[get1DIndexFast(x,y-1,shape)]   : 0;
+        double sxym = (x > 0 && y > 0) ? sum[get1DIndexFast(x-1,y-1,shape)] : 0;
         
         double val = value + sxm + sym - sxym;
-        sum.setAbs(get1DIndex(x, y, shape), val);  // Fast
+        sum[index] = val;  // Fast
  	}
 
 	
@@ -142,10 +133,11 @@ public class SummedAreaTable implements IDataset {
 		if (box[0] % 2 == 0) throw new Exception("Box first dim is not odd!");
 		if (box[1] % 2 == 0) throw new Exception("Box second dim is not odd!");
 
-		final DoubleDataset fano = new DoubleDataset(shape);
 		
 		// Compute some things to save FPOs
 		int n = box[0]*box[1]; // Save a FPO inside loop.
+		
+		final double[] fano = new double[shape[0]*shape[1]];
 		
 		int r1 = (int)Math.floor(box[0]/2d); // for instance 3->1, 5->2, 7->3 
 		int r2 = (int)Math.floor(box[1]/2d); // for instance 3->1, 5->2, 7->3 
@@ -154,24 +146,26 @@ public class SummedAreaTable implements IDataset {
 
 		if (sum2==null) createSummedTable(image, true);
 
-		final PositionIterator it = new PositionIterator(shape);
-		while(it.hasNext()) {
+		int[] point  = new int[]{0,0};
+		int[] coords = new int[]{0,0,0,0};
+		
+		for (int i = 0; i < fano.length; i++) {
 			
 			// Point from the iterator
-			int[] point   = it.getPos();
+			fillNDPositionFromShape(i, shape, point);
 			
 			// Save FPO by calculating coords once per pixel and
 			// passing to getBoxVarianceInternal and getBoxMeanInternal
-			int [] coords = createCoordsInternal(point, radii);
+			fillCoordsInternal(point, radii, coords);
 			
 			// These save some floating points
 			double vari = getBoxVarianceInternal(coords, n);
 			double mean = getBoxMeanInternal(coords, n);
 			
 			// Use concrete because fast
-			fano.setAbs(get1DIndex(point[0], point[1], shape), vari/mean);
+			fano[i] = vari/mean;
 		}
-		return fano;
+		return new DoubleDataset(fano, shape);
 	}
 
 	/**
@@ -255,17 +249,17 @@ public class SummedAreaTable implements IDataset {
 	 * @param coords Coordinates of box: x1,y1,x2,y2
 	 * @return the sum of a region
 	 */
-	private static final double getBoxSumInternal(Dataset sum, int[] coords, int[] shape) {
+	private static final double getBoxSumInternal(double[] sum, int[] coords, int[] shape) {
 
 		int minx = coords[0];
 		int miny = coords[1];
 		int maxx = coords[2];
 		int maxy = coords[3];
 		
-		double A = (minx > 0 && miny > 0) ? sum.getElementDoubleAbs(get1DIndex(minx-1, miny-1, shape)) : 0d;
-		double B = (miny > 0)             ? sum.getElementDoubleAbs(get1DIndex(maxx, miny-1, shape))   : 0d;
-		double C = (minx > 0)             ? sum.getElementDoubleAbs(get1DIndex(minx-1, maxy, shape))   : 0d;
-		double D = sum.getElementDoubleAbs(get1DIndex(maxx, maxy, shape));
+		double A = (minx > 0 && miny > 0) ? sum[get1DIndexFast(minx-1, miny-1, shape)] : 0d;
+		double B = (miny > 0)             ? sum[get1DIndexFast(maxx, miny-1, shape)]   : 0d;
+		double C = (minx > 0)             ? sum[get1DIndexFast(minx-1, maxy, shape)]   : 0d;
+		double D = sum[get1DIndexFast(maxx, maxy, shape)];
 		
 		return (D+A-B-C);
 	}
@@ -277,22 +271,15 @@ public class SummedAreaTable implements IDataset {
 	 * @param shape
 	 * @return index
 	 */
-	private final static int get1DIndex(int i, int j, int[]shape) {
-		if (i < 0) {
-			i += shape[0];
-		}
-		if (i < 0 || i >= shape[0]) {
-			throw new ArrayIndexOutOfBoundsException(i);
-		}
-		if (j < 0) {
-			j += shape[1];
-		}
-		if (j < 0 || j >= shape[1]) {
-			throw new ArrayIndexOutOfBoundsException(j);
-		}
+	private final static int get1DIndexFast(int i, int j, int[]shape) {
 		return i*shape[1] + j;
 	}
 
+	private final static void fillNDPositionFromShape(int n, int[] shape, int[] pos) {
+		pos[1] = n % shape[1];
+		n /= shape[1];
+		pos[0] = n;
+	}
 	
 	/**
 	 * Get the variance for a given box.
@@ -345,225 +332,6 @@ public class SummedAreaTable implements IDataset {
 		return (1d/n)*(s2 - (Math.pow(s1, 2d)/n));
 	}
 
-	@Override
-	public int[] getShape() {
-		return shape;
-	}
-
-	@Override
-	public String getName() {
-		return sum.getName();
-	}
-
-	@Override
-	public void setName(String name) {
-		sum.setName(name);
-	}
-
-	@Override
-	public void setStringFormat(Format format) {
-		sum.setStringFormat(format);
-	}
-
-	@Override
-	public int getItemsize() {
-		return sum.getItemsize();
-	}
-
-	@Override
-	public <T extends MetadataType> List<T> getMetadata(Class<T> clazz) throws Exception {
-		return sum.getMetadata(clazz);
-	}
-
-	@Override
-	public Object getObject(int... pos) {
-		return sum.getObject(pos);
-	}
-
-	@Override
-	public String getString(int... pos) {
-		return sum.getString(pos);
-	}
-
-	@Override
-	public Class<?> elementClass() {
-		return sum.elementClass();
-	}
-
-	@Override
-	public int getElementsPerItem() {
-		return sum.getElementsPerItem();
-	}
-
-	@Override
-	public double getDouble(int... pos) {
-		return sum.getDouble(pos);
-	}
-
-	@Override
-	public int getSize() {
-		return sum.getSize();
-	}
-
-	@Override
-	public long getLong(int... pos) {
-		return sum.getLong(pos);
-	}
-
-	@Override
-	public float getFloat(int... pos) {
-		return sum.getFloat(pos);
-	}
-
-	@Override
-	public int getInt(int... pos) {
-		return sum.getInt(pos);
-	}
-
-	@Override
-	public void setShape(int... shape) {
-		sum.setShape(shape);
-	}
-
-	@Override
-	public short getShort(int... pos) {
-		return sum.getShort(pos);
-	}
-
-	@Override
-	public byte getByte(int... pos) {
-		return sum.getByte(pos);
-	}
-
-	@Override
-	public int getRank() {
-		return sum.getRank();
-	}
-
-	@Override
-	public boolean getBoolean(int... pos) {
-		return sum.getBoolean(pos);
-	}
-
-	@Override
-	public void set(Object obj, int... pos) {
-		sum.set(obj, pos);
-	}
-
-	@Override
-	public IDataset getSlice(int[] start, int[] stop, int[] step) {
-		return sum.getSlice(start, stop, step);
-	}
-
-	@Override
-	public void resize(int... newShape) {
-		sum.resize(newShape);
-	}
-
-	@Override
-	public IDataset squeezeEnds() {
-		return sum.squeezeEnds();
-	}
-
-	@Override
-	public IDataset squeeze() {
-		return sum.squeeze();
-	}
-
-	@Override
-	public IDataset squeeze(boolean onlyFromEnds) {
-		return sum.squeeze(onlyFromEnds);
-	}
-
-	@Override
-	public Object mean(boolean... switches) {
-		return sum.mean(switches);
-	}
-
-	@Override
-	public IDataset getSlice(IMonitor monitor, int[] start, int[] stop, int[] step) throws Exception {
-		return sum.getSlice(monitor, start, stop, step);
-	}
-
-	@Override
-	public Number min(boolean... switches) {
-		return sum.min(switches);
-	}
-
-	@Override
-	public IDataset getSlice(Slice... slice) {
-		return sum.getSlice(slice);
-	}
-
-	
-	@Override public Number max(boolean... switches) {
-		return sum.max(switches);
-	}
-
-	@Override public IDataset getSlice(SliceND slice) {
-		return sum.getSlice(slice);
-	}
-
-	@Override public IDataset getSlice(IMonitor monitor, Slice... slice) throws Exception {
-		return sum.getSlice(monitor, slice);
-	}
-
-	@Override public int[] minPos() {
-		return sum.minPos();
-	}
-
-	@Override public int[] maxPos() {
-		return sum.maxPos();
-	}
-
-	@Override public IDataset clone() {
-		return sum.clone();
-	}
-
-	@Override public IDataset getSlice(IMonitor monitor, SliceND slice) throws Exception {
-		return sum.getSlice(monitor, slice);
-	}
-
-	@Override public IMetadata getMetadata() {
-		return sum.getMetadata();
-	}
-
-	@Override public IDataset getSliceView(int[] start, int[] stop, int[] step) {
-		return sum.getSliceView(start, stop, step);
-	}
-
-	@Override public IDataset getSliceView(Slice... slice) {
-		return sum.getSliceView(slice);
-	}
-
-	@Override public IDataset getSliceView(SliceND slice) {
-		return sum.getSliceView(slice);
-	}
-
-	@Override public ILazyDataset getTransposedView(int... axes) {
-		return sum.getTransposedView(axes);
-	}
-
-	@Override public <T extends MetadataType> void addMetadata(T metadata) {
-		sum.addMetadata(metadata);
-	}
-
-	@Override public <T extends MetadataType> void setMetadata(T metadata) {
-		sum.setMetadata(metadata);
-	}
-
-	@Override public <T extends MetadataType> void clearMetadata(Class<T> clazz) {
-		sum.clearMetadata(clazz);
-	}
-
-	@Override public void setError(Serializable errors) {
-		sum.setError(errors);
-	}
-
-	@Override public ILazyDataset getError() {
-		return sum.getError();
-	}
-	
 	
 	/**
 	 * private static final so that compiler will inline it
@@ -583,10 +351,12 @@ public class SummedAreaTable implements IDataset {
 		int h = box[1];
 		int r1 = (int)Math.floor(w/2d); // for instance 3->1, 5->2, 7->3 
 		int r2 = (int)Math.floor(h/2d); // for instance 3->1, 5->2, 7->3 
-	    return createCoordsInternal(point, new int[]{r1, r2});
+		final int[] coords = new int[]{0,0,0,0};
+	    fillCoordsInternal(point, new int[]{r1, r2}, coords);
+	    return coords;
 	}
 	
-	private final int[] createCoordsInternal(int[] point, int[] radii)  {
+	private final void fillCoordsInternal(int[] point, int[] radii, int[] coords)  {
 
 		int x = point[0];
 		int y = point[1];
@@ -604,7 +374,18 @@ public class SummedAreaTable implements IDataset {
 		int maxy = y+r2;
 		if (maxy>=shape[1]) maxy = shape[1]-1;
 		
-		return new int[]{minx, miny, maxx, maxy};
+		coords[0] = minx;
+		coords[1] = miny;
+		coords[2] = maxx;
+		coords[3] = maxy;
+	}
+
+	public int[] getShape() {
+		return shape;
+	}
+
+	public double getDouble(int i, int j) {
+		return sum[get1DIndexFast(i, j, shape)];
 	}
 
 }
