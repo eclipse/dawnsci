feee931eb795ea6caac2ee6525062df9f4db0547
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2015-07-15 16:50:36 +0100

    Added new ICompositeTrace example, more work required.

--- org.eclipse.dawnsci.plotting.examples/src/org/eclipse/dawnsci/plotting/examples/CompositeExample.java
+++ org.eclipse.dawnsci.plotting.examples/src/org/eclipse/dawnsci/plotting/examples/CompositeExample.java
@@ -12,9 +12,12 @@
 package org.eclipse.dawnsci.plotting.examples;
 
 import java.io.File;
+import java.util.Arrays;
 
 import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
+import org.eclipse.dawnsci.analysis.dataset.impl.AbstractDataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.Maths;
 import org.eclipse.dawnsci.analysis.dataset.impl.Random;
 import org.eclipse.dawnsci.plotting.api.PlotType;
 import org.eclipse.dawnsci.plotting.api.trace.ICompositeTrace;
@@ -23,12 +26,12 @@ import org.eclipse.dawnsci.plotting.examples.util.BundleUtils;
 import org.eclipse.swt.widgets.Composite;
 
 /**
- * A basic view which plots image (2D) data.
+ * A view which plots several image (2D) data together on the same pliotting system.
  * 
  * This view uses the services available from plotting.api and 
  * analysis.io
  * 
- * @author fcp94556
+ * @author Matthew Gerring
  *
  */
 public class CompositeExample extends PlotExample {
@@ -53,10 +56,23 @@ public class CompositeExample extends PlotExample {
 			// Now we overlay a random image at a location
 			final IDataset rand  = Random.rand(0, 100, new int[]{500,500});
 			IImageTrace    front = system.createImageTrace("rand");
-			front.setData(rand, null, false);
+			IDataset x = Maths.add(AbstractDataset.arange(rand.getShape()[1], AbstractDataset.FLOAT), 0);
+			IDataset y = Maths.add(AbstractDataset.arange(rand.getShape()[0], AbstractDataset.FLOAT), 0);
+			front.setData(rand, Arrays.asList(x,y), false);
 			front.setAlpha(100);
-			
 			comp.add(front, 1);
+			
+			// Add Duke
+//			final File loc1 = new File(BundleUtils.getBundleLocation(Activator.PLUGIN_ID), "duke_football.jpg");
+//			final IDataset dduke = service.getDataset(loc1.getAbsolutePath(), new IMonitor.Stub());
+//
+//			IImageTrace    duke = system.createImageTrace("duke");
+//			IDataset x = Maths.add(AbstractDataset.arange(dduke.getShape()[1], AbstractDataset.FLOAT), 100);
+//			IDataset y = Maths.add(AbstractDataset.arange(dduke.getShape()[0], AbstractDataset.FLOAT), 200);
+//			duke.setData(dduke, Arrays.asList(x,y), false);
+//			duke.setAlpha(120);
+//			comp.add(duke, 2);
+			
 		
 			system.addTrace(comp);
 			
