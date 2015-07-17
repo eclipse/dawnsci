5a860492049fc4f2e3791740953fc4ad7690d03f
Jacob Filik <jacob.filik@diamond.ac.uk>
2015-07-17 17:10:12 +0100

    Update to composite example

--- org.eclipse.dawnsci.plotting.examples/src/org/eclipse/dawnsci/plotting/examples/CompositeExample.java
+++ org.eclipse.dawnsci.plotting.examples/src/org/eclipse/dawnsci/plotting/examples/CompositeExample.java
@@ -18,7 +18,6 @@ import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
 import org.eclipse.dawnsci.analysis.dataset.impl.AbstractDataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.Maths;
-import org.eclipse.dawnsci.analysis.dataset.impl.Random;
 import org.eclipse.dawnsci.plotting.api.PlotType;
 import org.eclipse.dawnsci.plotting.api.trace.ICompositeTrace;
 import org.eclipse.dawnsci.plotting.api.trace.IImageTrace;
@@ -47,20 +46,36 @@ public class CompositeExample extends PlotExample {
 			final IDataset image = service.getDataset(loc.getAbsolutePath(), new IMonitor.Stub());
 			// NOTE IMonitor is an alternative to IProgressMonitor which cannot be seen in the data layer.
 			
+			final IDataset sub = image.getSlice(null,null,new int[]{4,4});
+			
 			ICompositeTrace comp = system.createCompositeTrace("composite1");
 			IImageTrace     back = system.createImageTrace(getFileName());
-			back.setData(image, null, false);
+			
+			IDataset x = Maths.add(AbstractDataset.arange(sub.getShape()[1], AbstractDataset.FLOAT), 0).imultiply(4).iadd(100);
+			IDataset y = Maths.add(AbstractDataset.arange(sub.getShape()[0], AbstractDataset.FLOAT), 0).imultiply(4).iadd(100);
+			
+			back.setData(sub, Arrays.asList(x,y), false);
 			
 			comp.add(back, 0);
 			
+			IDataset med = image.getSlice(new int[]{424,494},new int[]{1357,1084},new int[]{2,2});
+			x = AbstractDataset.arange(med.getShape()[1], AbstractDataset.FLOAT).imultiply(2).iadd(494).iadd(100);
+			y = AbstractDataset.arange(med.getShape()[0], AbstractDataset.FLOAT).imultiply(2).iadd(424).iadd(100);
+			IImageTrace    mid = system.createImageTrace("mid");
+			mid.setData(med, Arrays.asList(x,y), false);
+			mid.setAlpha(255);
+			comp.add(mid, 1);
+			
+			
 			// Now we overlay a random image at a location
-			final IDataset rand  = Random.rand(0, 100, new int[]{500,500});
+			IDataset small = image.getSlice(new int[]{424,494},new int[]{831,1475},new int[]{1,1});
+//			final IDataset small = image.getSlice(new int[]{494,424},new int[]{1475,831},new int[]{1,1});
 			IImageTrace    front = system.createImageTrace("rand");
-			IDataset x = Maths.add(AbstractDataset.arange(rand.getShape()[1], AbstractDataset.FLOAT), 0);
-			IDataset y = Maths.add(AbstractDataset.arange(rand.getShape()[0], AbstractDataset.FLOAT), 0);
-			front.setData(rand, Arrays.asList(x,y), false);
-			front.setAlpha(100);
-			comp.add(front, 1);
+			x = AbstractDataset.arange(small.getShape()[1], AbstractDataset.FLOAT).iadd(494).iadd(100);
+			y = AbstractDataset.arange(small.getShape()[0], AbstractDataset.FLOAT).iadd(424).iadd(100);
+			front.setData(small, Arrays.asList(x,y), false);
+			front.setAlpha(255);
+			comp.add(front, 2);
 			
 			// Add Duke
 //			final File loc1 = new File(BundleUtils.getBundleLocation(Activator.PLUGIN_ID), "duke_football.jpg");
