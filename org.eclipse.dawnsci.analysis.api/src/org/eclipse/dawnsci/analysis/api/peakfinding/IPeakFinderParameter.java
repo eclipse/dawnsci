87735497cabeaabdcc76d9754b5140268d8b6b0a
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-07-19 23:49:22 +0100

    Moves IPeakFinderParameter from u.a.d.s.analysis.peakfinding.peakfinders

---
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/peakfinding/IPeakFinderParameter.java
@@ -0,0 +1,36 @@
+package org.eclipse.dawnsci.analysis.api.peakfinding;
+
+public interface IPeakFinderParameter {
+	
+	/**
+	 * equals() and hashcode() should be overridden since it is important that
+	 * there is only one parameter with each name
+	 */
+	
+	/**
+	 * Get the name of the parameter
+	 * @return String name of parameter
+	 */
+	public String getName();
+
+	/**
+	 * Get the boolean indicating whether this should be an integer parameter
+	 * or not
+	 * @return Boolean indicating if parameter is an integer
+	 */
+	public boolean isInt();
+	
+	/**
+	 * Gert value of the parameter
+	 * @return Number value of the parameter
+	 */
+	public Number getValue();
+
+	/**
+	 * Set the value of the parameter, checking whether the supplied type is
+	 * consistent with the expected isInt value
+	 * @param value New value of the parameter
+	 * @throws Exception If a double was supplied when an integer was expected
+	 */
+	public void setValue(Number value) throws Exception;
+}
