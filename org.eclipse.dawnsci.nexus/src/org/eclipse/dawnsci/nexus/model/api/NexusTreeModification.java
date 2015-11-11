b4e311cf982fd21ebad81ddf41bc7b98010d02ff
Peter Chang <peter.chang@diamond.ac.uk>
2015-11-11 09:28:44 +0000

    Add missing copyrights

--- org.eclipse.dawnsci.nexus/src/org/eclipse/dawnsci/nexus/model/api/NexusTreeModification.java
+++ org.eclipse.dawnsci.nexus/src/org/eclipse/dawnsci/nexus/model/api/NexusTreeModification.java
@@ -1,8 +1,19 @@
+/*-
+ *******************************************************************************
+ * Copyright (c) 2015 Diamond Light Source Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Matthew Dickie - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
 package org.eclipse.dawnsci.nexus.model.api;
 
 /**
  * Abstract interface for all object that can cause a nexus tree to be modified. 
- * @author wgp76868
  */
 public interface NexusTreeModification {
 
