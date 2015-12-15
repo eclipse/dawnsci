6c121210c5575cac66793f27cec09a215d5e19ae
Jacob Filik <jacob.filik@diamond.ac.uk>
2015-12-15 17:11:12 +0000

    Set to use swmr unconditionally on file open

--- org.eclipse.dawnsci.hdf5/src/org/eclipse/dawnsci/hdf5/HDF5FileFactory.java
+++ org.eclipse.dawnsci.hdf5/src/org/eclipse/dawnsci/hdf5/HDF5FileFactory.java
@@ -196,7 +196,7 @@ public class HDF5FileFactory {
 						} else {
 							access.writeable = writeable;
 							if (new File(cPath).exists()) {
-								int a = writeable ? HDF5Constants.H5F_ACC_RDWR : (withLatestVersion ? (HDF5Constants.H5F_ACC_RDONLY | HDF5Constants.H5F_ACC_SWMR_READ) : HDF5Constants.H5F_ACC_RDONLY);
+								int a = writeable ? HDF5Constants.H5F_ACC_RDWR : (HDF5Constants.H5F_ACC_RDONLY | HDF5Constants.H5F_ACC_SWMR_READ);
 								fid = H5.H5Fopen(cPath, a, fapl);
 							} else if (!writeable) {
 								logger.error("File {} does not exist!", cPath);
