6e2c8e67327890a891b7402077fe351e1a129883
Peter Chang <peter.chang@diamond.ac.uk>
2016-01-07 10:09:14 +0000

    Add serial version ID

--- org.eclipse.dawnsci.hdf5/src/org/eclipse/dawnsci/hdf5/HDF5LazySaver.java
+++ org.eclipse.dawnsci.hdf5/src/org/eclipse/dawnsci/hdf5/HDF5LazySaver.java
@@ -23,6 +23,8 @@ import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
  */
 public class HDF5LazySaver extends HDF5LazyLoader implements ILazySaver, Serializable {
 
+	private static final long serialVersionUID = -5244067010482825423L;
+
 	private int[] maxShape;
 	private int[] chunks;
 	private Object fill;
