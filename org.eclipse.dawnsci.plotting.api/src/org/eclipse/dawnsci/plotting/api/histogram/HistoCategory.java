53152dfc24f1b98af9085f38d454ed6e5fc1825e
Baha El Kassaby <baha.elkassaby@gmail.com>
2016-02-05 15:07:33 +0000

    Modify histogram category enum

--- org.eclipse.dawnsci.plotting.api/src/org/eclipse/dawnsci/plotting/api/histogram/HistoCategory.java
+++ org.eclipse.dawnsci.plotting.api/src/org/eclipse/dawnsci/plotting/api/histogram/HistoCategory.java
@@ -12,12 +12,12 @@ package org.eclipse.dawnsci.plotting.api.histogram;
  */
 public enum HistoCategory {
 		ALL("All", "List of all colormaps"),
-		SEQUENTIAL1 ("Sequential 1", "Approximately monochromatic colormaps varying smoothly between two color tones---usually from low saturation (e.g. white) to high saturation (e.g. a bright blue)"), 
-		SEQUENTIAL2 ("Sequential 2", "Approximately monochromatic colormaps varying smoothly between two-three color tones"), 
-		UNIFORM ("Uniform", "Perceptually Uniform sequential colormaps"),
-		MISCELLANEOUS ("Miscellaneous", "Colormaps that don't fit into other categories"),
-		DIVERGENT ("Divergent", "Colour maps with median value (usually light in color) and which vary smoothly to two different color tones at high and low values"), 
-		QUALITATIVE ("Qualitative", "Colormaps which vary rapidly in color; useful for choosing a set of discrete colors");
+		SEQUENTIAL1 ("Sequential 1 (approximately monochromatic with variation between two colours)", "Approximately monochromatic colormaps varying smoothly between two color tones---usually from low saturation (e.g. white) to high saturation (e.g. a bright blue)"), 
+		SEQUENTIAL2 ("Sequential 2 (variation between two or more colours)", "Approximately monochromatic colormaps varying smoothly between two-three color tones"), 
+		UNIFORM ("Uniform (perceptually uniform sequential)", "Perceptually Uniform sequential colormaps"),
+		DIVERGENT ("Divergent (with median value)", "Colour maps with median value (usually light in color) and which vary smoothly to two different color tones at high and low values"), 
+		QUALITATIVE ("Qualitative (rapid variation in colours)", "Colormaps which vary rapidly in color; useful for choosing a set of discrete colors"),
+		MISCELLANEOUS ("Miscellaneous", "Colormaps that don't fit into other categories");
 
 		private final String name;
 		private final String description;
@@ -31,7 +31,7 @@ public enum HistoCategory {
 			HistoCategory[] states = values();
 			String[] names = new String[states.length];
 			for (int i = 0; i < states.length; i++) {
-				names[i] = states[i].name();
+				names[i] = states[i].getName();
 			}
 			return names;
 		}
@@ -39,7 +39,7 @@ public enum HistoCategory {
 		public static HistoCategory getCategory(String category) {
 			HistoCategory[] states = values();
 			for (int i = 0; i < states.length; i++) {
-				if (states[i].name().equals(category))
+				if (states[i].getName().equals(category))
 					return states[i];
 			}
 			return null;
