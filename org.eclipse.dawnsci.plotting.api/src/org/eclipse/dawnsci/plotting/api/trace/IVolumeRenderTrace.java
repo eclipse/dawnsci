58568e7f4df837b866e4ef8f211bfc6750b01984
Joel Ogden <joel.ogden@diamond.ac.uk>
2016-02-29 15:40:32 +0000

    Simple volume render trace.
    Signed-off-by: Joel Ogden <joel.ogden@diamond.ac.uk>

---
+++ org.eclipse.dawnsci.plotting.api/src/org/eclipse/dawnsci/plotting/api/trace/IVolumeRenderTrace.java
@@ -0,0 +1,41 @@
+/*-
+ *******************************************************************************
+ * Copyright (c) 2011, 2014 Diamond Light Source Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Matthew Gerring - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.dawnsci.plotting.api.trace;
+
+import java.awt.image.BufferedImage;
+
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+
+/**
+ * This trace is currently only available with Java8 and there is
+ * no DAWN executable based on Java8 available on the website.
+ * 
+ * Probably DAWN 2.0 in 2015 will be Java8 based.
+ * 
+ * @author Joel Ogden
+ *
+ */
+public interface IVolumeRenderTrace extends IImage3DTrace {
+
+	
+	/**
+	 * Plot a volume on the plotting system.
+	 * 
+	 * Requires the images for each plane in the volume
+	 * 
+	 * Thread safe
+	 * 
+	 * @param 
+	 */
+	public void setData(final IDataset data);
+	
+}
