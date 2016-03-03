7fec9c992eec8a0213a240316258d941fb15ece3
Matthew Gerring <matthew.gerring@gmail.com>
2016-03-03 14:50:25 +0000

    Created a simple p2 site to make sure that the compilation dependencies of dawnsci are public.
    Signed-off-by: Matthew Gerring <matthew.gerring@gmail.com>

---
+++ make.bat
@@ -0,0 +1,7 @@
+java -jar C:/Work/eclipse/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar ^
+-application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher ^
+-metadataRepository file:/C:/Work/daq-eclipse_git/eclipse/dawnsci/p2/ ^
+-artifactRepository file:/C:/Work/daq-eclipse_git/eclipse/dawnsci/p2/ ^
+-source ./repo/ ^
+-compress ^
+-publishArtifacts
