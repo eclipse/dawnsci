3889d83fa33f8d5786a9c46673688a85c118da30
Joel Ogden <joel.ogden@diamond.ac.uk>
2016-04-11 09:52:54 +0100

    Added volume example to the Example perspective.
    Signed-off-by: Joel Ogden <joel.a.ogden@gmail.com>

---
+++ org.eclipse.dawnsci.plotting.examples/src/org/eclipse/dawnsci/plotting/examples/VolumeExample.java
@@ -0,0 +1,66 @@
+/*-
+ * Copyright 2015 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.dawnsci.plotting.examples;
+
+import java.io.File;
+
+import org.eclipse.dawnsci.analysis.api.dataset.ILazyDataset;
+import org.eclipse.dawnsci.analysis.api.io.IDataHolder;
+import org.eclipse.dawnsci.plotting.api.PlotType;
+import org.eclipse.dawnsci.plotting.api.trace.IVolumeRenderTrace;
+import org.eclipse.dawnsci.plotting.examples.util.BundleUtils;
+import org.eclipse.swt.widgets.Composite;
+
+public class VolumeExample extends PlotExample {
+
+	@Override
+	protected void createExampleContent(Composite parent) {
+
+		try {
+			// We create a basic plot
+			system.createPlotPart(parent, "Volume Example", getViewSite()
+					.getActionBars(), PlotType.VOLUME, this);
+			system.setPlotType(PlotType.VOLUME);
+			File loc = new File(
+					BundleUtils.getBundleLocation(Activator.PLUGIN_ID),
+					getFileName());
+
+			String fp = loc.getAbsolutePath();
+			IDataHolder dh = service.getData(fp, true, null);
+
+			ILazyDataset lazyDataset = dh.getLazyDataset("/entry/result/data");
+
+			final IVolumeRenderTrace volume = system
+					.createVolumeRenderTrace("Volume1");
+
+			volume.setData(
+					lazyDataset.getShape(),
+					lazyDataset.getSlice(
+							new int[] { 0, 0, 0 },
+							lazyDataset.getShape(), 
+							new int[] { 2, 2, 2 }),
+							0.42, 
+							0.61, 
+							new double[] { 0.2, 8.0 }, 
+							new double[] { 0.1, 10000 });
+
+			system.addTrace(volume);
+
+		} catch (Throwable ne) {
+			ne.printStackTrace(); // Or your favourite logging.
+		}
+
+	}
+
+	protected String getFileName() {
+		return "volumeExample.nxs";
+	}
+
+}
