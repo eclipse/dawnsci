d35f6d07a50d37fee2c27aeed22faf6d310c7201
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2016-10-04 09:46:45 +0100

    Added DawnsciPlottingClassRegistry, for example client and server.
    Signed-off-by: Martin Gaughran <martin.gaughran@diamond.ac.uk>

---
+++ org.eclipse.dawnsci.plotting.api/src/org/eclipse/dawnsci/plotting/api/classregistry/DawnsciPlottingClassRegistry.java
@@ -0,0 +1,47 @@
+/*-
+ * Copyright © 2016 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eclipse.dawnsci.plotting.api.classregistry;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.dawnsci.analysis.api.persistence.IClassRegistry;
+import org.eclipse.dawnsci.plotting.api.region.IRegion;
+
+public class DawnsciPlottingClassRegistry implements IClassRegistry {
+
+	private static final Map<String, Class<?>> idToClassMap;
+	static {
+		Map<String, Class<?>> tmp = new HashMap<String, Class<?>>();
+		
+		registerClass(tmp, IRegion.RegionType.class);		
+
+		idToClassMap = Collections.unmodifiableMap(tmp);
+	}
+	
+	private static void registerClass(Map<String, Class<?>> map, Class<?> clazz) {
+		map.put(clazz.getSimpleName(), clazz);
+	}
+
+	@Override
+	public Map<String, Class<?>> getIdToClassMap() {
+		return idToClassMap;
+	}
+}
