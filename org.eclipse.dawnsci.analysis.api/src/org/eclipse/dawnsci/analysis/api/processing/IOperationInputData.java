ee1784fcc47341e788da396f66638dec87b32701
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-10-26 17:17:04 +0100

    IOperationInputData: add setInputData
    Probably to be reverted at some point

--- org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/processing/IOperationInputData.java
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/processing/IOperationInputData.java
@@ -5,9 +5,12 @@ import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.model.IOperationModel;
 import org.eclipse.january.dataset.IDataset;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
 public interface IOperationInputData {
 
 	public IDataset getInputData();
+	public void setInputData(IDataset data);
 	
 	public IOperation<? extends IOperationModel, ? extends OperationData> getCurrentOperation();
 	
