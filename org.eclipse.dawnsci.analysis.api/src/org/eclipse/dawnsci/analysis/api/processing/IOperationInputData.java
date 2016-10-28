bd3dd6062169a6767c9708a78084f20f346a2ed7
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-10-28 13:29:12 +0100

    IOperationInputData: support list of operations

--- org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/processing/IOperationInputData.java
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/processing/IOperationInputData.java
@@ -1,17 +1,16 @@
 package org.eclipse.dawnsci.analysis.api.processing;
 
+import java.util.List;
+
 import org.eclipse.dawnsci.analysis.api.processing.IOperation;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.model.IOperationModel;
 import org.eclipse.january.dataset.IDataset;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
-
 public interface IOperationInputData {
 
 	public IDataset getInputData();
-	public void setInputData(IDataset data);
 	
-	public IOperation<? extends IOperationModel, ? extends OperationData> getCurrentOperation();
+	public List<IOperation<? extends IOperationModel, ? extends OperationData>> getCurrentOperations();
 	
 }
