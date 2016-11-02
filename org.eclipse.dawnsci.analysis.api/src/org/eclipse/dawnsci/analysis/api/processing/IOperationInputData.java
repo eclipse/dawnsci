4ec202c0cc603b14ba7ab240d90a3d93030f25cd
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-11-02 14:35:05 +0000

    IOperationInputData: return rawtype

--- org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/processing/IOperationInputData.java
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/processing/IOperationInputData.java
@@ -3,14 +3,13 @@ package org.eclipse.dawnsci.analysis.api.processing;
 import java.util.List;
 
 import org.eclipse.dawnsci.analysis.api.processing.IOperation;
-import org.eclipse.dawnsci.analysis.api.processing.OperationData;
-import org.eclipse.dawnsci.analysis.api.processing.model.IOperationModel;
 import org.eclipse.january.dataset.IDataset;
 
 public interface IOperationInputData {
 
 	public IDataset getInputData();
 	
-	public List<IOperation<? extends IOperationModel, ? extends OperationData>> getCurrentOperations();
+	@SuppressWarnings("rawtypes")
+	public List<IOperation> getCurrentOperations();
 	
 }
