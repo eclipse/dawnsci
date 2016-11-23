d3fa9c2d100a3d3bb983aaffdf21a76f092e61eb
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2016-11-23 17:20:02 +0000

    Fix up json headers
    Signed-off-by: Matthew Gerring <matthew.gerring@gmail.com>

--- org.eclipse.dawnsci.json.test/src/org/eclipse/dawnsci/json/test/testobject/ITestTypeRegistered.java
+++ org.eclipse.dawnsci.json.test/src/org/eclipse/dawnsci/json/test/testobject/ITestTypeRegistered.java
@@ -1,26 +1,14 @@
 /*-
- * Copyright © 2016 Diamond Light Source Ltd.
+ * Copyright 2016 Diamond Light Source Ltd.
  *
- * This file is part of GDA.
- *
- * GDA is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License version 3 as published by the Free
- * Software Foundation.
- *
- * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along
- * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 
 package org.eclipse.dawnsci.json.test.testobject;
 
-/**
- *
- */
 public interface ITestTypeRegistered {
 
 	public void setString(String str);
