eclipse/dawnsci

commit e2cf15fd945b7b80a5817a72cfc3d15d9be23e15

SCI-5390: Removal of HObject and IHierchicalDataFile from H5 Editor

org.eclipse.dawnsci.hdf5.editor/src/org/eclipse/dawnsci/hdf5/editor/H5ValuePage.java
@@ -1,6 +1,6 @@
 /*-
  *******************************************************************************
- * Copyright (c) 2011, 2014 Diamond Light Source Ltd.
+ * Copyright (c) 2011, 2017 Diamond Light Source Ltd.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,24 +8,22 @@
  *
  * Contributors:
  *    Matthew Gerring - initial API and implementation and/or initial documentation
+ *    Baha El-Kassaby - Removal of IHierchicalDataFile and HObject usage
  *******************************************************************************/ 
 package org.eclipse.dawnsci.hdf5.editor;
 
-import java.lang.reflect.Array;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
-import javax.swing.tree.DefaultMutableTreeNode;
-
+import org.eclipse.dawnsci.analysis.api.tree.Attribute;
 import org.eclipse.dawnsci.analysis.api.tree.DataNode;
+import org.eclipse.dawnsci.analysis.api.tree.GroupNode;
 import org.eclipse.dawnsci.analysis.api.tree.Node;
 import org.eclipse.dawnsci.analysis.api.tree.NodeLink;
-import org.eclipse.dawnsci.analysis.api.tree.TreeAdaptable;
-import org.eclipse.dawnsci.hdf.object.HierarchicalDataFactory;
-import org.eclipse.dawnsci.hdf.object.HierarchicalDataUtils;
-import org.eclipse.dawnsci.hdf.object.IHierarchicalDataFile;
 import org.eclipse.january.DatasetException;
 import org.eclipse.january.dataset.IDataset;
 import org.eclipse.january.dataset.ILazyDataset;
@@ -38,7 +36,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IPartListener;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbench;
@@ -49,12 +46,6 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.Page;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import hdf.object.Attribute;
-import hdf.object.Dataset;
-import hdf.object.Datatype;
-import hdf.object.Group;
-import hdf.object.HObject;
 
 public class H5ValuePage extends Page  implements ISelectionListener, IPartListener {
 
@@ -188,47 +179,12 @@ public class H5ValuePage extends Page  implements ISelectionListener, IPartListe
 	}
 
 	public void updateObjectSelection(Object sel)  throws Exception{
-
-		if (sel instanceof DefaultMutableTreeNode) {
-			final DefaultMutableTreeNode node = (DefaultMutableTreeNode)sel;
-			final Object                 ob   = node.getUserObject();
-			if (ob instanceof HObject) {
-				createH5Value((HObject)ob);
-			}
- 		} else if (sel instanceof H5Path) { // Might be nexus part.
-			try {
-				final String path   = ((H5Path) sel).getPath();
-				final IEditorPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-				if (part instanceof IH5Editor) {
-					final String filePath = ((IH5Editor)part).getFilePath();
-					
-					IHierarchicalDataFile file = null;
-					try {
-						file = HierarchicalDataFactory.getReader(filePath);
-						final HObject ob = (HObject)file.getData(path);
-						createH5Value(ob);
-					} finally {
-						if (file!=null) file.close();
-					}
-				}
-			} catch (Exception ne) {
-				logger.error(ne.getMessage()); // Not serious, no need for stack.
-			}
- 		} else if (sel instanceof TreeAdaptable) {
- 			NodeLink nl = ((TreeAdaptable) sel).getNodeLink();
- 			if (nl != null) {
-	 			if (nl.isDestinationGroup())
-	 				label.setText("Group name of '" + nl.getName() + "' children:");
-	 			else if (nl.isDestinationData())
-	 				label.setText("Dataset name of '" + nl.getName() + "' value:");
-	
-	 			sourceViewer.getTextWidget().setText(getNodeLinkValue(nl));
- 			} else {
- 				org.eclipse.dawnsci.analysis.api.tree.Attribute a = ((TreeAdaptable) sel).getAttribute();
-	 			if (a != null) {
-	 				sourceViewer.getTextWidget().setText(a.toString());
-	 			}
- 			}
+		if (sel instanceof Attribute) {
+			Attribute att = (Attribute) sel;
+			createAttributeValue(att);
+		} else if (sel instanceof NodeLink) {
+			NodeLink node = (NodeLink) sel;
+			createNodeValue(node);
 		}
 	}
 	
@@ -242,7 +198,6 @@ public class H5ValuePage extends Page  implements ISelectionListener, IPartListe
 			DataNode hd = (DataNode)node;
 			ILazyDataset lz = hd.getDataset();
 			if (lz.getRank()==1 && lz.getShape()[0]<500) {
-								
 				try {
 					IDataset data = lz.getSlice();
 					if (Number.class.isAssignableFrom(data.getElementClass())) {
@@ -268,95 +223,82 @@ public class H5ValuePage extends Page  implements ISelectionListener, IPartListe
 		return buf.toString();
 	}
 	
+	@SuppressWarnings("unused")
 	private static boolean isNumericalDType(int dtype) {
 		return (dtype <= 8 );
 	}
 
+	private void createAttributeValue(Attribute att) throws Exception {
+	}
 
-	private void createH5Value(HObject ob) throws Exception {
-		
-		if (ob instanceof Dataset) {
-			final Dataset  set   = (Dataset)ob;
-			final Datatype dType = set.getDatatype();
-			final long[] shape = (long[])set.getDims();
-			
+	private void createNodeValue(NodeLink link) throws Exception {
+		Node node = link.getDestination();
+		if (node instanceof DataNode) {
+			final DataNode set = (DataNode) node;
+			final String dType = set.getTypeName();
+			final int[] shape = set.getDataset().getShape();
+
 			final StringBuilder buf = new StringBuilder();
-			if (dType!=null && dType.getDatatypeClass()==Datatype.CLASS_STRING) {
-				label.setText("Dataset name of '"+set.getName()+"' value:");
-				final long id = set.open();
+			if (dType != null && dType.equals("STRING")) {
+				label.setText("Dataset name of '" + link.getName() + "' value:");
 				try {
-					final String[] value = (String[])set.read();
-					buf.append(value[0]);
+					final String value = set.getString();
+					buf.append(value);
 				} catch (Exception e) {
 					// Ignored
-				} finally {
-					set.close(id);
 				}
 			} else if (shape!=null) {
-				label.setText("Dataset name of '"+set.getName()+"' shape:");
+				label.setText("Dataset name of '"+link.getName()+"' shape:");
 				buf.append(Arrays.toString(shape).trim());
 				
 				long size = shape[0];
 				for (int i = 1; i < shape.length; i++) size*=shape[i];
 				if (size<10) {
 					buf.append("\n\nValue:\n");
-					
-					final Object data = set.getData();
-				    int length = Array.getLength(data);
-				    buf.append('[');
-				    for (int i = 0; i < length; ++i) {
-					    buf.append(Array.get(data, i));
-					    if (i<length-1) buf.append(',');
-				    }
-				    buf.append(']');
+					String value = getNodeLinkValue(link);
+					buf.append(value);
 				}
-				
 			} else {
-				label.setText("Dataset name of '"+set.getName()+"'");
+				label.setText("Dataset name of '"+link.getName()+"'");
 			}
-			appendAttributes(set, buf);
+			appendAttributes("\nAttributes", set, buf);
 			sourceViewer.getTextWidget().setText(buf.toString());
-			
-		} if (ob instanceof Group) {
-			final Group  grp   = (Group)ob;
-			
-			label.setText("Group name of '"+grp.getName()+"' children:");
-			final List members = grp.getMemberList();
-			
-			final StringBuilder buf = (members!=null) ? new StringBuilder(members.toString()) : new StringBuilder();
-			final Group par = grp.getParent();
-			if (par.isRoot())  appendAttributes("\nFile Attributes", par, buf);
-			
-			appendAttributes(par.isRoot() ? "Attributes" : "\nAttributes", grp, buf);
-			sourceViewer.getTextWidget().setText(buf.toString());
-
-		}
-	}
-	
-	private void appendAttributes(HObject set, StringBuilder buf) throws Exception {
-
-		appendAttributes("\nAttributes", set, buf);
-	}
-	
-	private void appendAttributes(String title, HObject set, StringBuilder buf) throws Exception {
-		
-		final List meta = set.getMetadata();
-		if (meta==null || meta.isEmpty()) return;
-		
-		buf.append("\n"+title+":\n");
-		for (Object attribute : meta) {
-			
-			if (attribute instanceof Attribute) {
-				Attribute a = (Attribute)attribute;
-				buf.append(a.getName());
-				buf.append(" = ");
-				buf.append(HierarchicalDataUtils.extractValue(a.getValue()));
-				buf.append("\n");
+		} else if (node instanceof GroupNode) {
+			final GroupNode grp = (GroupNode) node;
+			label.setText("Group name of '" + link.getName() + "' children:");
+			final List<String> members = new ArrayList<String>(grp.getNumberOfNodelinks());
+			Iterator<String> nameIterator = grp.getNodeNameIterator();
+			while (nameIterator.hasNext()) {
+				String name = (String) nameIterator.next();
+				members.add(name);
 			}
+			final StringBuilder buf = (members != null) ? new StringBuilder(members.toString()) : new StringBuilder();
+
+			GroupNode source = (GroupNode)link.getSource();
+			Attribute hdf5version = source.getAttribute("HDF5_Version");
+			// if it is the root node
+			if (hdf5version != null) {
+				appendAttributes("\nFile Attributes", source, buf);
+				appendAttributes("Attributes", grp, buf);
+			} else {
+				appendAttributes("\nAttributes", grp, buf);
+			}
+			sourceViewer.getTextWidget().setText(buf.toString());
 		}
 	}
 
-
+	private void appendAttributes(String title, Node set, StringBuilder buf) throws Exception {
+		Iterator<? extends Attribute> attIterator = set.getAttributeIterator();
+		if (attIterator.hasNext())
+			buf.append("\n" + title + ":\n");
+		while (attIterator.hasNext()) {
+			Attribute a = (Attribute) attIterator.next();
+			buf.append(a.getName());
+			buf.append(" = ");
+			buf.append("[" + a.getFirstElement() + "]");
+			buf.append("\n");
+		}
+	}
 
 	private static IWorkbenchPage getActivePage() {
 		final IWorkbench bench = PlatformUI.getWorkbench();
@@ -365,6 +307,5 @@ public class H5ValuePage extends Page  implements ISelectionListener, IPartListe
 		if (window == null) return null;
 		return window.getActivePage();
 	}
-	
 
 }

org.eclipse.dawnsci.hdf5.editor/META-INF/MANIFEST.MF
@@ -10,14 +10,14 @@ Require-Bundle: org.eclipse.core.runtime,
  org.eclipse.jface;bundle-version="3.6.1",
  org.eclipse.jface.text;bundle-version="3.7.1",
  org.eclipse.core.resources;bundle-version="3.6.0",
- org.eclipse.dawnsci.hdf5,
  org.eclipse.january;bundle-version="1.0.0",
  org.eclipse.dawnsci.analysis.api;bundle-version="1.0.0",
- hdf.object;bundle-version="2.9.0",
- org.eclipse.ui.ide;bundle-version="3.11.0"
+ org.eclipse.ui.ide;bundle-version="3.11.0",
+ org.eclipse.dawnsci.nexus
 Bundle-RequiredExecutionEnvironment: JavaSE-1.7
 Bundle-ActivationPolicy: lazy
 Import-Package: org.eclipse.ui,
  org.eclipse.ui.dialogs,
  org.slf4j;version="[1.7.2,2.0.0)"
 Export-Package: org.eclipse.dawnsci.hdf5.editor
+Service-Component: OSGI-INF/*.xml

org.eclipse.dawnsci.hdf5.editor/OSGI-INF/loaderService.xml
@@ -1,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<scr:component xmlns:scr="http://www.osgi.org/xmlns/scr/v1.1.0" immediate="true" name="HDF5 editor Loader Service">
+   <implementation class="org.eclipse.dawnsci.hdf5.editor.ServiceHolder"/>
+   <reference bind="setLoaderService" cardinality="1..1" interface="org.eclipse.dawnsci.analysis.api.io.ILoaderService" name="ILoaderService" policy="static"/>
+</scr:component>

org.eclipse.dawnsci.hdf5.editor/OSGI-INF/nexusFileFactory.xml
@@ -1,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<scr:component xmlns:scr="http://www.osgi.org/xmlns/scr/v1.1.0" name="HDF5 editor Nexus File factory">
+   <implementation class="org.eclipse.dawnsci.hdf5.editor.ServiceHolder"/>
+   <reference bind="setNexusFileFactory" cardinality="1..1" interface="org.eclipse.dawnsci.nexus.INexusFileFactory" name="INexusFileFactory" policy="static"/>
+</scr:component>

org.eclipse.dawnsci.hdf5.editor/build.properties
@@ -4,5 +4,6 @@ bin.includes = META-INF/,\
                .,\
                plugin.xml,\
                icons/,\
-               schema/
+               schema/,\
+               OSGI-INF/
 jre.compilation.profile = JavaSE-1.7

org.eclipse.dawnsci.hdf5.editor/src/org/eclipse/dawnsci/hdf5/editor/H5ContentProvider.java
@@ -1,6 +1,6 @@
 /*-
  *******************************************************************************
- * Copyright (c) 2011, 2014 Diamond Light Source Ltd.
+ * Copyright (c) 2011, 2017 Diamond Light Source Ltd.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,11 +8,21 @@
  *
  * Contributors:
  *    Matthew Gerring - initial API and implementation and/or initial documentation
+ *    Baha El-Kassaby - Removal of IHierchicalDataFile and HObject usage
  *******************************************************************************/ 
 package org.eclipse.dawnsci.hdf5.editor;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.swing.tree.TreeNode;
 
+import org.eclipse.dawnsci.analysis.api.tree.Attribute;
+import org.eclipse.dawnsci.analysis.api.tree.DataNode;
+import org.eclipse.dawnsci.analysis.api.tree.GroupNode;
+import org.eclipse.dawnsci.analysis.api.tree.Node;
+import org.eclipse.dawnsci.analysis.api.tree.NodeLink;
 import org.eclipse.jface.viewers.ILazyTreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
@@ -39,19 +49,75 @@ public class H5ContentProvider implements ILazyTreeContentProvider {
 
 	@Override
 	public void updateElement(Object parent, int index) {
-		TreeNode node = (TreeNode) parent;
-		if (treeViewer != null && index < node.getChildCount()) {
-			TreeNode element = node.getChildAt(index);
-			treeViewer.replace(parent, index, element);
-			updateChildCount(element, -1);
+		if (parent instanceof NodeLink) {
+			NodeLink node = (NodeLink) parent;
+			int size = 0;
+			String name = "";
+			GroupNode groupnode = null;
+			List<String> names = null;
+			NodeLink link = null;
+			if(node.getDestination() instanceof DataNode) {
+				DataNode datanode = (DataNode) node.getDestination();
+				size = datanode.getNumberOfAttributes();
+				Iterator<String> attributesNameIt = datanode.getAttributeNameIterator();
+				names = new ArrayList<String>(datanode.getNumberOfAttributes());
+				while (attributesNameIt.hasNext()) {
+					String attribute = (String) attributesNameIt.next();
+					names.add(attribute);
+				}
+				Iterator<? extends Attribute> attributesIt = datanode.getAttributeIterator();
+				List<Attribute> attributes = new ArrayList<Attribute>(datanode.getNumberOfAttributes());
+				while (attributesIt.hasNext()) {
+					Attribute attribute = (Attribute) attributesIt.next();
+					attributes.add(attribute);
+				}
+				Object att = attributes.get(index);
+				if (att instanceof NodeLink) {
+					link = (NodeLink) attributes.get(index);
+					if (treeViewer != null && index < size) {
+						name = names.get(index);
+						treeViewer.replace(parent, index, link);
+						updateChildCount(link, -1);
+					}
+				} else if (att instanceof Attribute) {
+					Attribute attribute = (Attribute)att;
+					if (treeViewer != null && index < size) {
+						name = names.get(index);
+						treeViewer.replace(parent, index, attribute);
+						updateChildCount(link, -1);
+					}
+				}
+			}
+			if (node.getDestination() instanceof GroupNode) {
+				groupnode = (GroupNode) node.getDestination();
+				size = groupnode.getNumberOfNodelinks();
+				names = new ArrayList<String>(groupnode.getNames());
+				if (treeViewer != null && index < size) {
+					name = names.get(index);
+					link = groupnode.findNodeLink(name);
+					treeViewer.replace(parent, index, link);
+					updateChildCount(link, -1);
+				}
+			}
 		}
 	}
 
 	@Override
 	public void updateChildCount(Object element, int currentChildCount) {
-		TreeNode node = (TreeNode) element;
-		int size = node.getChildCount();
-		if (treeViewer != null)
+		NodeLink link = (NodeLink) element;
+		int size = 0;
+		Node destination = link != null ? link.getDestination() : null;
+		if (destination != null) {
+			if (destination instanceof DataNode) {
+				DataNode datanode = (DataNode) link.getDestination();
+				size = datanode.getNumberOfAttributes();
+			}
+			if (destination instanceof GroupNode) {
+				GroupNode groupnode = (GroupNode) link.getDestination();
+				size = groupnode.getNumberOfNodelinks();
+			}
+		}
+		if (treeViewer != null && element != null)
 			treeViewer.setChildCount(element, size);
 	}
 

org.eclipse.dawnsci.hdf5.editor/src/org/eclipse/dawnsci/hdf5/editor/H5Editor.java
@@ -1,6 +1,6 @@
 /*-
  *******************************************************************************
- * Copyright (c) 2011, 2014 Diamond Light Source Ltd.
+ * Copyright (c) 2011, 2017 Diamond Light Source Ltd.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,18 +8,19 @@
  *
  * Contributors:
  *    Matthew Gerring - initial API and implementation and/or initial documentation
+ *    Baha El-Kassaby - Removal of IHierchicalDataFile and HObject usage
  *******************************************************************************/ 
 package org.eclipse.dawnsci.hdf5.editor;
-
-import javax.swing.tree.TreeNode;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.dawnsci.hdf.object.HierarchicalDataFactory;
-import org.eclipse.dawnsci.hdf.object.IHierarchicalDataFile;
+import org.eclipse.dawnsci.analysis.api.io.IDataHolder;
+import org.eclipse.dawnsci.analysis.api.tree.GroupNode;
+import org.eclipse.dawnsci.analysis.api.tree.NodeLink;
+import org.eclipse.dawnsci.analysis.api.tree.Tree;
 import org.eclipse.jface.viewers.IContentProvider;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeViewer;
@@ -45,17 +46,16 @@ import org.slf4j.LoggerFactory;
 public class H5Editor extends EditorPart implements IReusableEditor, IH5Editor {
 
 	public static final String ID = "org.eclipse.dawnsci.hdf5.editor.raw.tree";
-	
-    private static final Logger logger = LoggerFactory.getLogger(H5Editor.class);
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(H5Editor.class);
+
 	private TreeViewer tree;
-	
+
 	@Override
 	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
-		
 		setSite(site);
 		super.setInput(input);
-        setPartName(input.getName());
+		setPartName(input.getName());
 	}
 
 	@Override
@@ -63,7 +63,7 @@ public class H5Editor extends EditorPart implements IReusableEditor, IH5Editor {
 		super.setInput(input);
 		setPartName(input.getName());
 		refresh();
-	}	
+	}
 
 	/**
 	 * Get the file path from a FileStoreEditorInput. Removes any "file:"
@@ -149,19 +149,20 @@ public class H5Editor extends EditorPart implements IReusableEditor, IH5Editor {
 		
 		try {
 			// NOTE This file may get closed externally
-	    	IHierarchicalDataFile file = HierarchicalDataFactory.getReader(getFilePath(getEditorInput()));
-	        try {
-	
-				final TreeNode root = file.getNode();
-				tree.getTree().setItemCount(root.getChildCount());
+			IDataHolder dh = ServiceHolder.getLoaderService().getData(getFilePath(getEditorInput()), null);
+			try {
+				Tree hdftree = dh.getTree();
+				NodeLink root = hdftree.getNodeLink();
+				GroupNode group = (GroupNode)root.getDestination();
+				int numitem = group.getNumberOfNodelinks();
+				tree.getTree().setItemCount(numitem);
 				tree.setContentProvider(new H5ContentProvider());
-				tree.setLabelProvider(new H5LabelProvider(file));
+				tree.setLabelProvider(new H5LabelProvider());
 				tree.setInput(root);
 				tree.expandToLevel(1);
-				        	
 			} catch (Exception e) {
 				logger.error("Cannot open h5 file "+getFilePath(getEditorInput()), e);
-	        } 
+			} 
 		} catch (Exception neOther) {
 			logger.error("Cannot open H5 file!", neOther);
 		}
@@ -188,9 +189,9 @@ public class H5Editor extends EditorPart implements IReusableEditor, IH5Editor {
 		return false;
 	}
 
-    /**
-     * The adapter IContentProvider gives the value of the H5Dataset
-     */
+	/**
+	 * The adapter IContentProvider gives the value of the H5Dataset
+	 */
 	public Object getAdapter(final Class clazz) {
 
 		if (clazz == IContentProvider.class) {

org.eclipse.dawnsci.hdf5.editor/src/org/eclipse/dawnsci/hdf5/editor/H5Filter.java
@@ -1,6 +1,6 @@
 /*-
  *******************************************************************************
- * Copyright (c) 2011, 2014 Diamond Light Source Ltd.
+ * Copyright (c) 2011, 2017 Diamond Light Source Ltd.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,16 +8,13 @@
  *
  * Contributors:
  *    Matthew Gerring - initial API and implementation and/or initial documentation
+ *    Baha El-Kassaby - Removal of HObject usage
  *******************************************************************************/ 
 package org.eclipse.dawnsci.hdf5.editor;
 
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeNode;
-
+import org.eclipse.dawnsci.analysis.api.tree.NodeLink;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
-
-import hdf.object.HObject;
 
 class H5Filter extends ViewerFilter {
 
@@ -33,21 +30,21 @@ class H5Filter extends ViewerFilter {
 		if (searchString == null || searchString.length() == 0) {
 			return true;
 		}
+		if (element instanceof NodeLink) {
+			NodeLink link = (NodeLink) element;
+			final String name = link.getName();
 
-		final TreeNode node   = (TreeNode)element;
-		final HObject  object = (HObject)((DefaultMutableTreeNode)node).getUserObject();
-		final String name     = object.getName();
-		
-		if (name==null || "".equals(name)) return true;
+			if (name == null || "".equals(name))
+				return true;
 
-		if (name.matches(searchString)) {
-			return true;
+			if (name.matches(searchString)) {
+				return true;
+			}
+			if (name.matches(searchString)) {
+				return true;
+			}
+
 		}
-		if (name.matches(searchString)) {
-			return true;
-		}
-
-
 		return false;
 	}
 

commit 1e9b870abad43028a1d6f39a847764bd2442d9c7

Fix H5ValuePage to show values
Broken by commit e2cf15fd945b7b80a5817a72cfc3d15d9be23e15

org.eclipse.dawnsci.hdf5.editor/src/org/eclipse/dawnsci/hdf5/editor/H5ValuePage.java
@@ -24,6 +24,7 @@ import org.eclipse.dawnsci.analysis.api.tree.DataNode;
 import org.eclipse.dawnsci.analysis.api.tree.GroupNode;
 import org.eclipse.dawnsci.analysis.api.tree.Node;
 import org.eclipse.dawnsci.analysis.api.tree.NodeLink;
+import org.eclipse.dawnsci.analysis.api.tree.TreeAdaptable;
 import org.eclipse.january.DatasetException;
 import org.eclipse.january.dataset.IDataset;
 import org.eclipse.january.dataset.ILazyDataset;
@@ -128,12 +129,13 @@ public class H5ValuePage extends Page  implements ISelectionListener, IPartListe
 			this.lastSelection = (StructuredSelection)selection;
 			final Object sel = lastSelection.getFirstElement();
 			
-			updateObjectSelection(sel);				
-			
-			sourceViewer.refresh();
-			label.getParent().layout(new Control[]{label, sourceViewer.getTextWidget()});
-			
-			return;
+			if (sel instanceof TreeAdaptable) {
+				updateObjectSelection((TreeAdaptable) sel);
+				
+				sourceViewer.refresh();
+				label.getParent().layout(new Control[]{label, sourceViewer.getTextWidget()});
+				return;
+			}
 		}
 		
 		clear();
@@ -178,13 +180,15 @@ public class H5ValuePage extends Page  implements ISelectionListener, IPartListe
 		
 	}
 
-	public void updateObjectSelection(Object sel)  throws Exception{
-		if (sel instanceof Attribute) {
-			Attribute att = (Attribute) sel;
-			createAttributeValue(att);
-		} else if (sel instanceof NodeLink) {
-			NodeLink node = (NodeLink) sel;
-			createNodeValue(node);
+	public void updateObjectSelection(TreeAdaptable sel)  throws Exception{
+		Attribute attr = sel.getAttribute();
+		if (attr != null) {
+			createAttributeValue(attr);
+		} else {
+			NodeLink node = sel.getNodeLink();
+			if (node != null) {
+				createNodeValue(node);
+			}
 		}
 	}
 	
@@ -229,6 +233,9 @@ public class H5ValuePage extends Page  implements ISelectionListener, IPartListe
 	}
 
 	private void createAttributeValue(Attribute att) throws Exception {
+		label.setText("Attribute name of '" + att.getName() + "' value:");
+
+		sourceViewer.getTextWidget().setText(att.toString());
 	}
 
 	private void createNodeValue(NodeLink link) throws Exception {