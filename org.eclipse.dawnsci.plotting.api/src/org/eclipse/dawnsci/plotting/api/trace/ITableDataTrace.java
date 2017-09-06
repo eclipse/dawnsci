2f8c116df848452891df4b6f4788d27f8dcec8a1
Jacob Filik <jacob.filik@diamond.ac.uk>
2017-09-06 13:27:51 +0100

    Marker interface for a trace containing table data

---
+++ org.eclipse.dawnsci.plotting.api/src/org/eclipse/dawnsci/plotting/api/trace/ITableDataTrace.java
@@ -0,0 +1,14 @@
+/*-
+ * Copyright 2017 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.dawnsci.plotting.api.trace;
+
+public interface ITableDataTrace extends ITrace {
+
+}
