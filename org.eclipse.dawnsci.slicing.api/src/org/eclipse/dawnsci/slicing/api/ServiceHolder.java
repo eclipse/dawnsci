f97c28cb70b0b845dc6ac9e82593631ecd30a8d0
Baha El Kassaby <baha.elkassaby@gmail.com>
2017-09-19 20:45:06 +0200

    Move hdf.object classes to their own plugin

---
+++ org.eclipse.dawnsci.slicing.api/src/org/eclipse/dawnsci/slicing/api/ServiceHolder.java
@@ -0,0 +1,30 @@
+/*-
+ * Copyright 2017 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.dawnsci.slicing.api;
+
+//import org.eclipse.dawnsci.nexus.INexusFileFactory;
+
+public class ServiceHolder {
+/*
+	private static INexusFileFactory nexusFileService;
+
+	public ServiceHolder() {
+		
+	}
+
+	public static INexusFileFactory getNexusFileService() {
+		return nexusFileService;
+	}
+
+	public static void setNexusFileService(INexusFileFactory nexusFileService) {
+		ServiceHolder.nexusFileService = nexusFileService;
+	}
+	*/
+}
