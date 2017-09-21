76da4e5d76cc5c60b2d2786db5c8e4caf5b6b1e3
Baha El Kassaby <baha.elkassaby@gmail.com>
2017-09-21 20:02:59 +0200

    HDF5 and hdf.object tests
    *Make ReadThreadTest equivalent class for dawnsci.hdf5
    *Move WriteThreadTest and ATTestBase back to hdf5 plugin
    
    Signed-off-by: Baha El-Kassaby <baha.elkassaby@gmail.com>

--- org.eclipse.dawnsci.hdf5.test/src/org/eclipse/dawnsci/hdf5/ReadThreadTest.java
+++ org.eclipse.dawnsci.hdf5.test/src/org/eclipse/dawnsci/hdf5/ReadThreadTest.java
@@ -7,40 +7,41 @@
  * http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.eclipse.dawnsci.hdf.object;
+package org.eclipse.dawnsci.hdf5;
 
-import org.eclipse.dawnsci.analysis.api.io.ScanFileHolderException;
+import java.util.Arrays;
+
+import org.eclipse.dawnsci.hdf5.HDF5File;
 import org.eclipse.dawnsci.hdf5.HDF5FileFactory;
 import org.eclipse.dawnsci.hdf5.HDF5Utils;
 import org.eclipse.january.dataset.Dataset;
 import org.eclipse.january.dataset.DatasetFactory;
 import org.eclipse.january.dataset.PositionIterator;
 import org.eclipse.january.dataset.ShapeUtils;
-import org.eclipse.january.dataset.SliceND;
 import org.junit.Test;
 
-public class WriteThreadTest extends AbstractThreadTestBase {
-	String file = "test-scratch/writethread.h5";
+public class ReadThreadTest extends AbstractThreadTestBase {
+	String file = "test-scratch/readthread.h5";
 	String group = "/entry/group1/group2";
-	int[] mshape = new int[] {-1, 128, 1024};
+	int[] shape = new int[] {512, 128, 1024};
 	int[] chunk = new int[] {1, 128, 1024};
 
 	@Test
-	public void testSingleThreadWrite() throws Throwable {
+	public void testSingleThreadRead() throws Throwable {
 		prepareForTest(file, 1);
 
 		doTestOfDataSet(0);
 	}
 
 	@Test
-	public void testTwoThreadsWrite() throws Throwable {
+	public void testTwoThreadsRead() throws Throwable {
 		prepareForTest(file, 2);
 
 		testWithNThreads(2);
 	}
 
 	@Test
-	public void testEightThreadsWrite() throws Throwable {
+	public void testEightThreadsRead() throws Throwable {
 		prepareForTest(file, 8);
 
 		testWithNThreads(8);
@@ -50,43 +51,39 @@ public class WriteThreadTest extends AbstractThreadTestBase {
 	protected void doTestOfDataSet(int index) throws Throwable {
 		String dataName = "data" + index;
 
-		int[] shape = chunk.clone();
-		int size = ShapeUtils.calcSize(shape);
-		Dataset data1 = DatasetFactory.createRange(size, Dataset.FLOAT64);
-		data1.setShape(shape);
-
-		shape[0] = 128;
 		PositionIterator iter = new PositionIterator(shape, 1, 2);
-		int[] pos = iter.getPos();
+		int[] start = iter.getPos();
+		int[] stop = shape.clone();
+		int[] step = new int[shape.length];
+		Arrays.fill(step, 1);
+		int[] nshape = chunk.clone();
 		long now;
 
-		int[] nshape = shape.clone();
-		nshape[0] = 1;
-		SliceND slice = new SliceND(nshape);
-		int[] start = slice.getStart();
-		int[] stop = slice.getStop();
-
+		iter.reset();
 		now = -System.nanoTime();
 		while (iter.hasNext()) {
-			for (int i = 0; i < 3; i++) {
-				start[i] = pos[i];
-			}
 			stop[0] = start[0] + 1;
-//			System.err.println("Writing to " + dataName + " @ " + start[0]);
-			HDF5Utils.setDatasetSlice(file, group, dataName, slice, data1);
+			HDF5Utils.loadDataset(file, dataName, start, nshape, step, Dataset.FLOAT64, 1, false);
 		}
-		now += System.nanoTime();
 		HDF5FileFactory.releaseFile(file);
+		now += System.nanoTime();
+
 		System.err.println("Thd for " + dataName + " took " + now*1e-9 + "s");
 	}
 
-	private void prepareForTest(String file, int nthreads) throws ScanFileHolderException {
-		int[] mshape = new int[] {-1, 128, 1024};
-
+	private void prepareForTest(String file, int nthreads) throws Exception {
 		HDF5FileFactory.deleteFile(file);
 
+		HDF5FileFactory.acquireFile(file, true);
+
+		HDF5File f = HDF5FileFactory.acquireFileAsNew(file);
+		int size = ShapeUtils.calcSize(shape);
+		Dataset data = DatasetFactory.createRange(size, Dataset.FLOAT64);
+		data.setShape(shape);
+
 		for (int i = 0; i < nthreads; i++) {
-			HDF5Utils.createDataset(file, group, "data" + i, chunk, mshape, chunk, Dataset.FLOAT64, null, false);
+			HDF5Utils.writeDataset(f, "data" + i, data);
 		}
+		HDF5FileFactory.releaseFile(file);
 	}
 }
