f91f341f80ccc1ea19aaeded78b820e25e38d3e9
Jacob Filik <jacob.filik@diamond.ac.uk>
2017-11-23 12:27:03 +0000

    add more logging to h5 to diagnose issues

--- org.eclipse.dawnsci.hdf5/src/org/eclipse/dawnsci/hdf5/HDF5Utils.java
+++ org.eclipse.dawnsci.hdf5/src/org/eclipse/dawnsci/hdf5/HDF5Utils.java
@@ -489,6 +489,7 @@ public class HDF5Utils {
 				final int lisize = isize >= 0 ? isize : type.isize;
 				data = DatasetFactory.zeros(lisize, count, ldtype);
 				Object odata = data.getBuffer();
+				logger.info(node + " has item " + lisize + " count " + Arrays.toString(count) + " type " + ldtype);
 
 				try {
 					if (type.isVariableLength) {
