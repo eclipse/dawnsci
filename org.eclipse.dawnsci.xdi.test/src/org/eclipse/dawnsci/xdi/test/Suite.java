733ff1327023d66d75f7dfd6eed6f7409005f2a1
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-04-12 12:07:12 +0100

    SCI-6656: add test suite for Travis-CI

---
+++ org.eclipse.dawnsci.xdi.test/src/org/eclipse/dawnsci/xdi/test/Suite.java
@@ -0,0 +1,12 @@
+package org.eclipse.dawnsci.xdi.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(org.junit.runners.Suite.class)
+@SuiteClasses({
+	XDITest.class,
+})
+public class Suite {
+
+}
